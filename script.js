afcc2e40c0a36841b1b003a8cdb578f3dc84e69a
simsj6 <simsj6@wwu.edu>
2026-04-22 23:54:14 -0700

    Make words/icons dynamic with JSON/JS + changing button colors

--- script.js
+++ script.js
@@ -15,3 +15,75 @@ const handleSend = () => {
 const handleClear = () => {
     input.value = "";
 }
+
+
+// POPULATE PAGE WITH WORDS
+
+// TODO: change img src links once have pictures
+
+// button colors
+const colors = [
+    "#14A25B",
+    "#2D7FD0",
+    "#7348B9",
+    "#D043A6",
+    "#FF4B75",
+    "#FF7F35",
+    "#FFBB3E"
+];
+
+
+// icons
+
+const table = fetch('./words.json')
+.then(res => res.json())
+.then(data => {
+    populateTable(data)
+})
+
+function populateTable(data) {
+    const categories = Object.keys(data);
+
+    const tableBody = document.getElementById('table-body');
+    const rowCount = 5;
+
+    tableBody.innerHTML = [...Array(rowCount)].map((row, index) => `
+        <tr>
+            ${categories.map((item, i) => {
+                const word = data[item][index];
+
+                return `
+                    <td>
+                        <button style="background-color: ${colors[i + 2]}" onclick="handleIconClick('${word.text}')">
+                            <img src="assets/icon.png" alt="${word.text}" width="35" height="35">
+                            <br />
+                            ${word.text}
+                        </button>
+                    </td>
+                `;
+            }).join('')} 
+        </tr>
+    `).join('');
+}
+
+
+// fanboys conjunctions
+
+const fanboys = fetch('./fanboys.json')
+    .then(res => res.json())
+    .then(data => {
+        populateFanboys(data.fanboys)
+    })
+
+function populateFanboys(data) {
+    const fanboysList = document.getElementsByClassName("fanboys-list")[0];
+
+    fanboysList.innerHTML = data.map((item, index) => (`
+        <li>
+          <button style="background-color: ${colors[0]}" onclick="handleIconClick('${item.text}')">
+              <img src="assets/icon.png" width=35rem height=35rem>
+              ${item.text}
+            </button>
+        </li>
+    `)).join('');
+}
